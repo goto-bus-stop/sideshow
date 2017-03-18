7ccd79c494408ffdb44157fe9abc38c8cd0e14b8
René Kooi <renee@kooi.me>
2017-03-18 16:42:12 +0100

    Format variables.js.

--- src/general/variables.js
+++ src/general/variables.js
@@ -1,45 +1,48 @@
-var globalObjectName = "Sideshow",
-  $window,
-  $body,
-  $document,
-  pollingDuration = 150,
-  longAnimationDuration = 600,
+var globalObjectName = "Sideshow";
+var $window;
+var $body;
+var $document;
+var pollingDuration = 150;
+var longAnimationDuration = 600;
+
+/**
+ * The main class for Sideshow
+ * 
+ * @class SS 
+ * @static
+ */
+var SS = {
   /**
-   * The main class for Sideshow
+   * The current Sideshow version
    * 
-   * @class SS 
-   * @static
+   * @property VERSION
+   * @type String
    */
-  SS = {
-    /**
-     * The current Sideshow version
-     * 
-     * @property VERSION
-     * @type String
-     */
-    get VERSION() {
-      return "0.4.3";
-    }
-  },
-  controlVariables = [],
-  flags = {
-    lockMaskUpdate: false,
-    changingStep: false,
-    skippingStep: false,
-    running: false
-  },
-  wizards = [],
-  currentWizard,
-  /**
-   * Possible statuses for an animation
-   * 
-   * @@enum AnimationStatus
-   */
-  AnimationStatus = jazz.Enum(
-    "VISIBLE",
-    "FADING_IN",
-    "FADING_OUT",
-    "NOT_DISPLAYED",
-    "NOT_RENDERED",
-    "TRANSPARENT"
-  );
+  get VERSION() {
+    return "0.4.3";
+  }
+};
+
+var controlVariables = [];
+var flags = {
+  lockMaskUpdate: false,
+  changingStep: false,
+  skippingStep: false,
+  running: false
+};
+var wizards = [];
+var currentWizard;
+
+/**
+ * Possible statuses for an animation
+ * 
+ * @@enum AnimationStatus
+ */
+var AnimationStatus = jazz.Enum(
+  "VISIBLE",
+  "FADING_IN",
+  "FADING_OUT",
+  "NOT_DISPLAYED",
+  "NOT_RENDERED",
+  "TRANSPARENT"
+);
