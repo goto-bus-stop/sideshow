7fc58d3a5d17001b476804cfd8f618baf104710d
René Kooi <renee@kooi.me>
2017-03-21 13:23:12 +0100

    Convert `Arrows` object to a class.

--- src/step/arrows.js
+++ src/step/arrows.js
@@ -13,165 +13,148 @@ function isVisible(el) {
  * Class representing all the current shown arrows
  *
  * @class Arrows
- * @static
  */
-var Arrows = {};
+class Arrows {
+  arrows = [];
 
-Arrows.arrows = [];
-
-/**
- * Clear the currently defined arrows
- *
- * @method clear
- * @static
- */
-Arrows.clear = function() {
-  this.arrows = [];
-};
-
-/**
- * Sets the targets for arrows to point
- *
- * @method setTargets
- * @static
- */
-Arrows.setTargets = function(targets, targetsChanged) {
-  if (typeof targets === "string") {
-    targets = document.querySelectorAll(targets);
+  /**
+   * Clear the currently defined arrows
+   *
+   * @method clear
+   */
+  clear() {
+    this.arrows = [];
   }
 
-  if (targets.length > 0) {
+  /**
+   * Sets the targets for arrows to point
+   *
+   * @method setTargets
+   */
+  setTargets(targets, targetsChanged) {
+    if (typeof targets === "string") {
+      targets = document.querySelectorAll(targets);
+    }
+
+    if (targets.length > 0) {
+      foreach(
+        el => {
+          const arrow = new Arrow();
+          arrow.target.$el = el;
+          if (isVisible(arrow.target.$el)) {
+            this.arrows.push(arrow);
+            arrow.onceVisible = true;
+          }
+        },
+        targets
+      );
+    } else if (!targetsChanged) {
+      throw new SSException("150", "Invalid targets.");
+    }
+  }
+
+  recreateDOMReferences() {
+    const parent = this.arrows[0] ? this.arrows[0].$el.parentNode : document.body;
+
     foreach(
-      el => {
-        const arrow = new Arrow();
-        arrow.target.$el = el;
-        if (isVisible(arrow.target.$el)) {
-          Arrows.arrows.push(arrow);
-          arrow.onceVisible = true;
-        }
+      arrow => {
+        remove(arrow.$el);
       },
-      targets
+      this.arrows
     );
-  } else if (!targetsChanged) {
-    throw new SSException("150", "Invalid targets.");
+
+    this.clear();
+    this.setTargets(currentWizard.currentStep.targets, true);
+    parent.appendChild(this.render());
+    this.positionate();
+    this.show();
   }
-};
 
-Arrows.recreateDOMReferences = function() {
-  const parent = this.arrows[0] ? this.arrows[0].$el.parentNode : document.body;
-
-  foreach(
-    arrow => {
-      remove(arrow.$el);
-    },
-    this.arrows
-  );
-
-  Arrows.clear();
-  Arrows.setTargets(currentWizard.currentStep.targets, true);
-  parent.appendChild(Arrows.render());
-  Arrows.positionate();
-  Arrows.show();
-};
-
-/**
- * Iterates over the arrows collection showing each arrow
- *
- * @method show
- * @static
- */
-Arrows.show = function() {
-  this.arrows.forEach(arrow => {
-    arrow.show();
-  });
-};
-
-/**
- * Iterates over the arrows collection hiding each arrow
- *
- * @method hide
- * @static
- */
-Arrows.hide = function() {
-  this.arrows.forEach(arrow => {
-    arrow.hide();
-  });
-};
-
-/**
- * Iterates over the arrows collection fading in each arrow
- *
- * @method fadeIn
- * @static
- */
-Arrows.fadeIn = function() {
-  this.arrows.forEach(arrow => {
-    arrow.fadeIn();
-  });
-};
-
-/**
- * Iterates over the arrows collection fading out each arrow
- *
- * @method fadeOut
- * @static
- */
-Arrows.fadeOut = function() {
-  this.arrows.forEach(registerFadeOut);
-
-  function registerFadeOut(arrow) {
-    arrow.fadeOut(() => {
-      arrow.destroy();
-    });
+  /**
+   * Iterates over the arrows collection showing each arrow
+   *
+   * @method show
+   */
+  show() {
+    foreach(arrow => arrow.show(), this.arrows);
   }
-};
 
-/**
- * Iterates over the arrows collection repositionating each arrow
- *
- * @method positionate
- * @static
- */
-Arrows.positionate = function() {
-  this.arrows.forEach(arrow => {
-    arrow.positionate();
-  });
-};
+  /**
+   * Iterates over the arrows collection hiding each arrow
+   *
+   * @method hide
+   */
+  hide() {
+    foreach(arrow => arrow.hide(), this.arrows);
+  }
 
-/**
- * Iterates over the arrows collection rendering each arrow
- *
- * @method render
- * @static
- */
-Arrows.render = function(arrowPosition = "top") {
-  const fragment = document.createDocumentFragment();
-  this.arrows.forEach(arrow => {
-    arrow.position = arrowPosition;
-    fragment.appendChild(arrow.render());
-  });
-  return fragment;
-};
+  /**
+   * Iterates over the arrows collection fading in each arrow
+   *
+   * @method fadeIn
+   */
+  fadeIn() {
+    foreach(arrow => arrow.fadeIn(), this.arrows);
+  }
 
-/**
- * A Polling function to check if arrows coordinates has changed
- *
- * @method pollForArrowsChanges
- */
-Arrows.pollForArrowsChanges = function() {
-  var brokenReference = false;
-  this.arrows.forEach(arrow => {
-    if (arrow.hasChanged()) {
-      arrow.positionate();
+  /**
+   * Iterates over the arrows collection fading out each arrow
+   *
+   * @method fadeOut
+   */
+  fadeOut() {
+    foreach(registerFadeOut, this.arrows);
+
+    function registerFadeOut(arrow) {
+      arrow.fadeOut(() => {
+        arrow.destroy();
+      });
     }
-    if (arrow.onceVisible && !isVisible(arrow.target.$el)) {
-      brokenReference = true;
-    }
-  });
-
-  if (brokenReference) {
-    this.recreateDOMReferences();
   }
-};
 
-export default Arrows;
+  /**
+   * Iterates over the arrows collection repositionating each arrow
+   *
+   * @method positionate
+   */
+  positionate() {
+    foreach(arrow => arrow.positionate(), this.arrows);
+  }
+
+  /**
+   * Iterates over the arrows collection rendering each arrow
+   *
+   * @method render
+   */
+  render(arrowPosition = "top") {
+    const fragment = document.createDocumentFragment();
+    foreach(arrow => {
+      arrow.position = arrowPosition;
+      fragment.appendChild(arrow.render());
+    }, this.arrows);
+    return fragment;
+  }
+
+  /**
+   * A Polling function to check if arrows coordinates has changed
+   *
+   * @method pollForArrowsChanges
+   */
+  pollForArrowsChanges() {
+    let brokenReference = false;
+    foreach(arrow => {
+      if (arrow.hasChanged()) {
+        arrow.positionate();
+      }
+      if (arrow.onceVisible && !isVisible(arrow.target.$el)) {
+        brokenReference = true;
+      }
+    }, this.arrows);
+
+    if (brokenReference) {
+      this.recreateDOMReferences();
+    }
+  }
+}
+
+export default new Arrows();
